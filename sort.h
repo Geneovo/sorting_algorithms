37571dcb0f68f1b18ff1300257e93502f25e1bff
Geneovo <bodjuvwederhie@gmail.com>
2024-01-23 15:02:15 -0800

    Sorting Algorithm

--- sort.h
+++ sort.h
@@ -18,6 +18,7 @@ typedef enum bool
 	false = 0,
 	true
 } bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -42,7 +43,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sort(listint **list);
+void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
