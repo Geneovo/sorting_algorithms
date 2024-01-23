7c798a4a6366aaa6f88efc7137cac0dc4315e4b0
Geneovo <bodjuvwederhie@gmail.com>
2024-01-23 05:13:59 -0800

    sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,33 +5,26 @@
  *
  * @array: The array to sort
  * @size: The size of the array
+ * Return: Nothing
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+	size_t i = 0, j = 0;
+	int swapped = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL || size == 0)
 		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (; i < size - 1; i++)
 	{
-		swapped = 0;
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = 1;
-
+				swapped = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = swapped;
 				print_array(array, size);
 			}
 		}
-
-		if (swapped == 0)
-			break;
 	}
 }
